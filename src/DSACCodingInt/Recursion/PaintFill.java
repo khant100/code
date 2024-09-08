3178c0cc4538e41001702fd7153d33160d8511ad
khant vyas <khantavyas@gmail.com>
2024-09-08 18:52:21 +0530

    fill paint code done

--- src/DSACCodingInt/Recursion/PaintFill.java
+++ src/DSACCodingInt/Recursion/PaintFill.java
@@ -1,5 +1,44 @@
 package DSACCodingInt.Recursion;
 
+
+/**
+ *
+ * Problem: Implement the "paint fill" function that one might see on many image
+ * editing programs. That is, given a screen(represented by a two-dimensional
+ * array of colors), a point, and a new color, fill in the surrounding area
+ * until the color changes from the original color.
+ *
+ */
+
+enum Color{
+    Black,White,Red;
+}
 public class PaintFill {
+
+    public boolean FillColour(Color[][] screen,int col, int raw,Color nColor){
+        if(screen[raw][col]==nColor){
+            return false;
+        }
+        return fill(screen,raw,col,screen[raw][col],nColor);
+
+    }
+
+    public boolean fill(Color[][] screen,int raw,int col,Color ocoler,Color ncoler){
+        if(raw>screen.length||raw<0||col<0||col>screen[raw].length){
+            return false;
+        }
+        if(screen[raw][col]==ocoler) {
+            screen[raw][col]=ncoler;
+            fill(screen, raw +1,col,ocoler,ncoler);
+            fill(screen, raw, col + 1,ocoler, ncoler);
+            fill(screen,  raw, col,  ocoler,  ncoler);
+            fill(screen,  raw,  col,  ocoler, ncoler);
+        }
+        return true;
+
+    }
+
+
+
 
 }
