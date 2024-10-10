c2710aea98f85d4d5563c5697dc273dbcdef8ad5
khant vyas <khantavyas@gmail.com>
2024-10-10 20:32:24 +0530

    leetcode medium

---
+++ src/LeetcodeRevist/ReverseWordInStrings.java
@@ -0,0 +1,30 @@
+package LeetcodeRevist;
+
+public class ReverseWordInStrings {
+
+    public String ReverseWords(String s){
+        String giv = s.trim();
+        String[] sen = giv.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=sen.length-1;i>=0;i--){
+
+            if(sen[i].length()>0){
+                sb.append(sen[i].trim());
+                sb.append(" ");
+            }
+        }
+
+      return  sb.toString().trim();
+    }
+
+
+
+
+    public static void main(String args[]){
+    ReverseWordInStrings rs = new ReverseWordInStrings();
+        System.out.println("the sky is blue->"+rs.ReverseWords("the sky is blue"));
+        System.out.println("a good   example->"+rs.ReverseWords("a good   example"));
+        System.out.println("  hello world  ->"+rs.ReverseWords("  hello world  "));
+
+    }
+}
