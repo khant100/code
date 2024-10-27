6628f7c3c1600c6fec1b5f9a00da88b694161f05
khant vyas <khantavyas@gmail.com>
2024-10-27 17:45:59 +0530

    start of DP

--- src/concept/DynamicProgramming/Fibonnachi.java
+++ src/concept/DynamicProgramming/Fibonnachi.java
@@ -8,6 +8,31 @@ public class Fibonnachi
 {
 
 
+    public int fibonacci(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+
+        int prev=1;
+        int prev0=0;
+        int res=0;
+        for(int i=2;i<=n;i++){
+            res = prev+prev0;
+            prev0=prev;
+            prev=res;
+            System.out.println("->"+prev);
+        }
+return prev;
+    }
+    public static void main(String args[]){
+
+        Fibonnachi f = new Fibonnachi();
+        System.out.println("Fibonnac"+f.fibonacci(5));
+
+    }
 
 
 }
