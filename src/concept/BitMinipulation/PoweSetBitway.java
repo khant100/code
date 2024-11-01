068c512c88a9e39fa831ce81f47ff474e9712c17
khant100 <khantavyas@gmail.com>
2024-11-01 18:55:38 +0530

    powerset with bitmanipulation

---
+++ src/concept/BitMinipulation/PoweSetBitway.java
@@ -0,0 +1,30 @@
+package concept.BitMinipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoweSetBitway {
+
+    public static void generatePowerSet(int[] arr,List<List<Integer>> result){
+
+        int len = 1<<arr.length;
+        for(int i=0;i<len;i++){
+            List<Integer> lis = new ArrayList<>();
+            for(int j=0;j<arr.length;j++){
+                if((i&(1<<j))!=0){
+                    lis.add(arr[j]);
+                }
+            }
+            result.add(lis);
+        }
+    }
+
+
+    public static void main(String args[]){
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = new ArrayList<>();
+        generatePowerSet(arr,ans);
+        ans.stream().forEach(x->System.out.println(x));
+
+    }
+}
