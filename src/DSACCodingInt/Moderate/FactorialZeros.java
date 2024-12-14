b1cf9b5530b3a44ac2089451cf88e291e684c4a5
khant100 <khantavyas@gmail.com>
2024-12-14 13:54:17 +0530

    factorial

--- src/DSACCodingInt/Moderate/FactorialZeros.java
+++ src/DSACCodingInt/Moderate/FactorialZeros.java
@@ -5,8 +5,39 @@ public class FactorialZeros {
         if(sum<3){
             return sum;
         }
+        return sum * fact(sum-1);
+    }
 
-        return sum
+    private static int findfac(int sum){
+        int count =0;
+        for(int i=2;i<=sum;i++){
+            count+=fact5(i);
+        }
+    return count;
+    }
+
+    private static int countZero(int sum){
+        int count =0;
+        for(int i=5;sum/i>0;i=i*5){
+            count+=sum/i;
+        }
+        return count;
+    }
+    private static int fact5(int i){
+        int count =0;
+        while(i%5==0){
+            count++;
+            i=i/5;
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+
+        for (int i = 1; i < 12; i++) {
+            System.out.println(i + "! (or " + fact(i) + ") has " + findfac(i) + " zeros");
+            System.out.println(i + "! (or " + fact(i) + ") has " + countZero(i) + " zeros");
+        }
 
     }
 
