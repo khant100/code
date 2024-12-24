abe8f28d115221be329fea8645a759f10eb86a6e
khant100 <khantavyas@gmail.com>
2024-12-24 17:49:40 +0530

    bipatart graph

--- src/concept/Graph/BiPartitGraph.java
+++ src/concept/Graph/BiPartitGraph.java
@@ -1,5 +1,8 @@
 package concept.Graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BiPartitGraph {
 //    There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1. You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to. More formally, for each v in graph[u], there is an undirected edge between node u and node v. The graph has the following properties:
 //
@@ -11,4 +14,37 @@ public class BiPartitGraph {
 //
  //   Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
    // Output: false
+
+    public static boolean isBiparte(int[][] gr){
+        int n = gr.length;
+        int[] colour = new int[n];
+
+        for (int node = 0; node < n; node++) {
+            if (colour[node] == 0) {
+                Queue<Integer> q = new LinkedList<>();
+                q.add(node);
+                colour[node] = 1;
+                while (!q.isEmpty()) {
+                    int cur = q.peek();
+                    q.remove();
+                    for (int ne : gr[cur]) {
+                        if (colour[ne] == 0) {
+                            colour[ne] = -colour[cur];
+                            q.add(ne);
+                        } else if (colour[ne] != -colour[cur]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        int[][] graph = {{1,2,3},{0,2},{0,1,3},{0,2}};
+        int[][] gra = {{1,3},{0,2},{1,3},{0,2}};
+        System.out.println(isBiparte(graph));
+        System.out.println(isBiparte(gra));
+    }
 }
