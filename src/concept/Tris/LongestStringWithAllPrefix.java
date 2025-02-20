708a01399ebe13e7472b44f1d7940398a8a803e5
khant100 <khantavyas@gmail.com>
2025-02-20 23:48:40 +0530

    tirs

--- src/concept/Tris/LongestStringWithAllPrefix.java
+++ src/concept/Tris/LongestStringWithAllPrefix.java
@@ -1,2 +1,88 @@
-package concept.Tris;public class LongestStringWithAllPrefix {
+package concept.Tris;
+
+
+import DSACCodingInt.Searching.BinarySearch;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Node3{
+    Node3[] links;
+    boolean flag;
+    Node3(){
+        links=new Node3[26];
+        flag=false;
+    }
+
+    public boolean cointainKey(char ch){
+        return links[ch-'a']!=null;
+    }
+
+    public Node3 get(char ch){
+        return links[ch-'a'];
+    }
+
+    public void put(char ch,Node3 node){
+        links[ch-'a']=node;
+    }
+
+    public boolean isEnd(){
+        return flag;
+    }
+
+    public void setEnd(){
+        flag=false;
+    }
+}
+
+class Tris0{
+    Node3 root =new Node3();
+    public void insert(String s){
+        Node3 node=root;
+        for(int i=0;i<s.length();i++){
+            if(!node.cointainKey(s.charAt(i))){
+                node.put(s.charAt(i),new Node3());
+            }
+            node=node.get(s.charAt(i));
+        }
+        node.setEnd();
+    }
+
+    public boolean containsAllString(String s){
+        boolean flag = true;
+        Node3 node3 =root;
+        for(int i=0;i<s.length();i++){
+            if(node3.cointainKey(s.charAt(i))){
+                node3=node3.get(s.charAt(i));
+                flag=flag&&node3.isEnd();
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+public class LongestStringWithAllPrefix {
+
+    public String completeString(int n,List<String> given){
+        Tris0 t = new Tris0();
+        String longest = "";
+        for(String s :given){
+        t.insert(s);
+        }
+        for(int i=0;i<given.size();i++){
+            if(t.containsAllString(given.get(i))){
+                if(given.get(i).length()>longest.length()||(given.get(i).length()==longest.length())&&(given.get(i).compareTo(longest)<0)){
+                    longest=given.get(i);
+                }
+            }
+        }
+        return longest==""?"None":longest;
+    }
+    public static void main(String[] args) {
+        LongestStringWithAllPrefix l = new LongestStringWithAllPrefix();
+        List<String> words = Arrays.asList("apple", "app", "ap", "apply", "bat", "bats", "ba");
+        System.out.println(l.completeString(words.size(), words)); // Expected output: "apply"
+    }
 }
--- src/concept/Tris/NumberOfUniqueSubString.java
+++ src/concept/Tris/NumberOfUniqueSubString.java
@@ -1,2 +1,84 @@
-package concept.Tris;public class NumberOfUniqueSubString {
+package concept.Tris;
+
+//Problem Statement: Implement a program that takes a string 'S' as input and returns the number
+//        of distinct substrings of the given string,
+//        including the empty substring. Use a trie data structure to accomplish this.
+class Node2{
+    Node2[] links;
+    boolean flag;
+
+    public boolean Containskey(char ch){
+        return links[ch-'a']!=null;
+    }
+
+    public Node2 get(char ch){
+        return links[ch-'a'];
+    }
+
+    public void put(char ch,Node2 node){
+        links[ch-'a']=node;
+    }
+
+    public boolean isEnd(){
+        return flag;
+    }
+    public Node2(){
+        links = new Node2[26];
+        flag=false;
+    }
+
+    public void setEnd(){
+        flag=true;
+    }
+
 }
+
+//class Tries2 {
+//    Node2 root;
+//
+//    Tries2() {
+//        root = new Node2();
+//    }
+//
+//    public void insert(String s) {
+//        Node2 node2 = root;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (!node2.Containskey(s.charAt(i))) {
+//                node2.put(s.charAt(i), new Node2());
+//
+//            }
+//            node2 = node2.get(s.charAt(i));
+//        }
+//        node2.setEnd();
+//    }
+//}
+public class NumberOfUniqueSubString {
+
+    public static int countDistinctSubstrings(String s) {
+        Node2 root = new Node2();
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            Node2 node = root;
+            for (int j = i; j < s.length(); j++) {
+                if (!node.Containskey(s.charAt(j))) {
+                    node.put(s.charAt(j),new Node2());
+                    count++;
+                }
+                node = node.get(s.charAt(j));
+            }
+
+        }
+        return count+1;
+    }
+
+    public static void main(String[] args) {
+        String s = "striver";
+        // Input string
+        System.out.println("Current String: " + s);
+        System.out.println("Number of distinct substrings: " + countDistinctSubstrings(s));
+        // Output the result
+    }
+}
+
